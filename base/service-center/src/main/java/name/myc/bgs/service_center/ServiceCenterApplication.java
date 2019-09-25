6a0349ee00c097bcc56027ce696caa53841fc7c5
myc <mycsoft@qq.com>
2019-09-25 22:08:26 +0800

    上传框架与注册中心

---
+++ base/service-center/src/main/java/name/myc/bgs/service_center/ServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package name.myc.bgs.service_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@Eureka
+@SpringBootApplication
+public class ServiceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCenterApplication.class, args);
+	}
+
+}
