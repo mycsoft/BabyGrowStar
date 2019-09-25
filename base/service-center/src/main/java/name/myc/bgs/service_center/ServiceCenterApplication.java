206c775da518b67829ec71f9362078d378206b2a
myc <mycsoft@qq.com>
2019-09-25 22:18:11 +0800

    注册中心配置完成。封版0.0.1

--- base/service-center/src/main/java/name/myc/bgs/service_center/ServiceCenterApplication.java
+++ base/service-center/src/main/java/name/myc/bgs/service_center/ServiceCenterApplication.java
@@ -2,7 +2,9 @@ package name.myc.bgs.service_center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//@Eureka
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceCenterApplication {
 
