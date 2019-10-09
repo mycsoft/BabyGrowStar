cef591287ff096b44b48d8b1ad3bed9f78134de2
myc <mycsoft@qq.com>
2019-10-09 13:58:06 +0800

    将配置中心加入到服务注册中心

--- base/config-server/src/main/java/name/myc/bgs/base/config_server/ConfigServerApplication.java
+++ base/config-server/src/main/java/name/myc/bgs/base/config_server/ConfigServerApplication.java
@@ -3,14 +3,15 @@ package name.myc.bgs.base.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 }
