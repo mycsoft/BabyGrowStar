e7806516e8ef14576e65daeda13fa64d03d771c9
myc <mycsoft@qq.com>
2019-10-10 11:16:33 +0800

    实现熔断监视器服务

--- bgs-star/src/main/java/name/myc/bgs/star/StarApplication.java
+++ bgs-star/src/main/java/name/myc/bgs/star/StarApplication.java
@@ -1,8 +1,10 @@
 package name.myc.bgs.star;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -26,6 +28,21 @@ public class StarApplication {
     @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+    
+    /**
+     * 熔断器数据监控接口.
+     * @return 
+     */
+    @Bean
+    public ServletRegistrationBean getHystrixStreamServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        //设置熔断器数据接口.
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
