e3cc346a7fac8715f67010e83bebc16ae31cdccd
myc <mycsoft@qq.com>
2019-10-10 09:19:36 +0800

    加入宝贝账本对象模型。

--- common/model/src/main/java/name/myc/bgs/common/model/Baby.java
+++ common/model/src/main/java/name/myc/bgs/common/model/Baby.java
@@ -6,32 +6,42 @@
 package name.myc.bgs.common.model;
 
 import java.util.Date;
-import java.util.List;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
+ * 宝贝. 也是宝贝账本。每个账号下可以有多个宝贝账本。 这是为了满足一个家庭可以有多个宝贝的需求。
  *
  * @author mayic
  */
 @Data
-@Document(collection = "account")
-public class Account {
+@Document("baby")
+public class Baby {
 
+    /**
+     * 主键
+     */
     @Id
     private String id;
-
-    private String weixin;
+    /**
+     * 主账号
+     */
+    private Account account;
+    /**
+     * 宝贝名称
+     */
     private String name;
+    /**
+     * 星星的最后数量
+     */
+    private int starCount;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 修改时间
+     */
     private Date modifyTime;
-    /**
-     * 孩子的id。
-     */
-    private List<String> babyIds;
-    /**
-     * 默认孩子的id.
-     */
-    private String defaultBaby;
 }
